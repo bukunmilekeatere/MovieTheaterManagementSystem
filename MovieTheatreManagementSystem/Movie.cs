e05dbc0046e1b9b5ac90f8718ffb81fb1c2a6112
bukunmilekeatere <113192947+bukunmilekeatere@users.noreply.github.com>
2024-03-22 20:41:58 -0500

    Update Movie.cs

--- MovieTheatreManagementSystem/Movie.cs
+++ MovieTheatreManagementSystem/Movie.cs
@@ -8,12 +8,14 @@ namespace MovieTheatreManagementSystem
 {
     internal class Movie
     {
+        // movie class property
         public int Id { get; set; }
         public string Title { get; set; }
         public string Genre { get; set; }
         public int ReleaseYear { get; set; }
         public int Duration { get; set; }
 
+        // constructor for movie class 
         public Movie(string title, string genre, int releaseYear, int duration, int id)
         {
             Title = title; 
@@ -23,6 +25,7 @@ namespace MovieTheatreManagementSystem
             Id = id;
         }
 
+        // overide tostring method to show info for movie
         public override string ToString()
         {
             return $"{Title, -10}\t{Genre,-10}\t{ReleaseYear,-10}\t{Duration,-10}";
