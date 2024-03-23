ffbadf31153fc9781ed5854e5c498300b832df29
Adeola-Y <113193008+Adeola-Y@users.noreply.github.com>
2024-03-22 19:43:43 -0500

    Update Venue.cs

--- MovieTheatreManagementSystem/Venue.cs
+++ MovieTheatreManagementSystem/Venue.cs
@@ -8,21 +8,23 @@ namespace MovieTheatreManagementSystem
 {
     internal class Venue
     {
+        // calling all our variables 
         public int Id { get; set; }
         public string Name { get; set; }
-
+        public int VenueId { get; set; }
         public List<Theater> Theaters { get; set; }
 
-        public Venue(int id, string name)
+        public Venue(int id, string name, int venueId)
         {
             Id = id;
             Name = name;
             Theaters = new List<Theater>();
+            this.VenueId = venueId;
         }
 
         public override string ToString()
         {
-            return $"{Id}\t{Name}\t";
+            return $"{Id}\t{Name}\t {VenueId}\t";
         }
     }
 }
